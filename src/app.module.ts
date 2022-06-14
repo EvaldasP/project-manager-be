2fbc64020aeda1353acb4dd23e955d007a2bfee3
Evaldas Pocius <evaldukas96@gmail.com>
2022-06-14 22:37:05 +0300

    add env variables for db password

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://Evaldas:zzz2001@projectmanager.lfisd3d.mongodb.net/?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: `mongodb+srv://Evaldas:${process.env.DB_PASSWORD}@projectmanager.lfisd3d.mongodb.net/?retryWrites=true&w=majority`,
+      }),
+    }),
+    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
