5dbde96d5d6192303fe74ecd56ee912c039407d0
Evaldas Pocius <evaldukas96@gmail.com>
2022-06-15 22:31:40 +0300

    JWT strategy initial

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
 })
 export class AuthModule {}
