947cca1ec173203d37c88bebde7587d23eaaf91c
Evaldas Pocius <evaldukas96@gmail.com>
2022-06-16 23:09:26 +0300

    Guard for project manager

---
+++ src/guards/roleManager.guard.ts
@@ -0,0 +1,23 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Roles } from 'src/users/users.model';
+
+@Injectable()
+export class roleManagerGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+
+    if (req.user.role !== Roles.ProjectManager) {
+      return true;
+    } else {
+      throw new UnauthorizedException();
+    }
+  }
+}
