ac57396149e8ba3bdacbee101341ca6c62ea2ed7
Evaldas Pocius <evaldukas96@gmail.com>
2022-07-24 22:02:16 +0300

    separate DTOs

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,14 +1,10 @@
 import { IsEnum, IsString, MinLength } from 'class-validator';
-import { Roles } from '../users.model';
 
-export class CreateUserDto {
+export class AuthCredentialsDto {
   @IsString()
   username: string;
 
   @IsString()
   @MinLength(8)
   password: string;
-
-  @IsEnum(Roles)
-  role: Roles;
 }
