1ad68aae8bef6ee6fa537b3c207716fe1092d8d6
Evaldas Pocius <evaldukas96@gmail.com>
2022-07-30 23:17:31 +0300

    chore: comments

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,36 +1,3 @@
-// import * as mongoose from 'mongoose';
-
-// export const UserSchema = new mongoose.Schema(
-//   {
-//     username: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//     },
-//     password: {
-//       type: String,
-//       required: true,
-//     },
-//     role: {
-//       type: String,
-//       required: true,
-//     },
-//   },
-//   { timestamps: true },
-// );
-
-// export enum Roles {
-//   Employee = 'Employee',
-//   ProjectManager = 'Project Manager',
-// }
-
-// export interface User extends mongoose.Document {
-//   _id: string;
-//   username: string;
-//   password: string;
-//   role: Roles;
-// }
-
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
